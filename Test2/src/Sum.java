f0f9b92acc340d4d8369c6cc4466fbb225718ffb
Goswami <2374796@cognizant.com>
2024-12-02 12:31:22 +0530

    First Commit

---
+++ Test2/src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(sum(5, 10));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
